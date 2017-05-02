451ea02cf2cbcfd1aec3fb8dc4dd8dc1efcc11e9
Brett Hoerner <brett@bretthoerner.com>
2017-05-02 14:59:40 -0500

    Add BaseTest.

--- sentry/src/test/java/io/sentry/DefaultSentryClientFactoryTest.java
+++ sentry/src/test/java/io/sentry/DefaultSentryClientFactoryTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.instanceOf;
 
-public class DefaultSentryClientFactoryTest {
+public class DefaultSentryClientFactoryTest extends BaseTest {
     @Test
     public void checkServiceLoaderProvidesFactory() throws Exception {
         ServiceLoader<SentryClientFactory> sentryFactories = ServiceLoader.load(SentryClientFactory.class);
